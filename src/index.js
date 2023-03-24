e10a584372621356549835b1b6c4fc7b0aaa702b
MORGANJASON <420morganjason@gmail.com>
2023-03-24 05:18:30 +0300

    Done with lab

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
 });
+
+const taskdescription = document.querySelector("#task-descripton");
+
+checkbox.addEventListener("click", taskdescriptionClick, false);
+
+function taskdescriptionClick(event) {
+  let warn = "preventDefault() won't let you check this!<br>";
+  document.getElementById("submittasks").innerHTML += warn;
+  // event.preventDefault();
+}
